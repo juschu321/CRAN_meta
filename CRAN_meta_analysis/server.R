ef848974ff86fe9ef214d8cf28acf084490baf56
juschu321 <juliaschuhmann@gmx.de>
2019-07-15 08:15:54 +0200

    developing the UI without background functions

--- CRAN_meta_analysis/server.R
+++ CRAN_meta_analysis/server.R
@@ -2,14 +2,21 @@ library(shiny)
 library(shinydashboard)
 
 server <- function(input, output) {
-  
- output$plot <- renderPlot(
-   {ggplot(data = test2) +
-       geom_line( aes (month, total, color = package))+
-       scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month",
-                    date_labels = "%Y - %m")
-     
-   })
- }
    
-
+   output$plot <- renderPlot(
+      {ggplot(data = test2) +
+            geom_line( aes (month, total, color = package))+
+            scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month",
+                         date_labels = "%Y - %m")
+         
+      })
+   
+   output$value <- renderPrint({ input$checkboxGroup })
+   
+   output$dateRangeText2 <- renderText({
+      paste("input$dateRange2 is", 
+            paste(as.character(input$dateRange2), collapse = " to ")
+      )
+   })
+   
+}
--- CRAN_meta_analysis/ui.R
+++ CRAN_meta_analysis/ui.R
@@ -2,30 +2,70 @@ library(shiny)
 library(shinydashboard)
 
 body <- dashboardBody(
+  
   fluidRow(
-    box(title = "dependencies", "plot dependencies"),
-    box(title = "linechart", width = 4, solidHeader = TRUE, status = "primary",
-      edgelist[1,1]
+  box(title = "filter", width = 8, solidHeader = TRUE, status = "primary",
+      box(title = "ctv"),
+      box(title = "package"),
+      box(title = "core"),
+      box(title = "subcategory")
+  )),
+  
+  
+  
+  fluidRow(
+    box(title = "plot: dependencies",width = 4, solidHeader = TRUE, status = "primary", "plot dependencies"),
+    box(title = "plot: linechart", width = 4, solidHeader = TRUE, status = "primary",
+        plotOutput("plot") 
     )
     
   ), 
   
   
   fluidRow(
-    
-    box(
-      title = "dependencies", width = 4, solidHeader = TRUE, status = "warning",
-      plotOutput("plot")
+     
+      box( title = "dependencies", width = 4, solidHeader = TRUE, status = "primary",
+        checkboxGroupInput(
+          "checkGroup",
+          label = "specify type of dependencies",
+          choices = list(
+            "imports" = 1,
+            "depends" = 2,
+            "suggests" = 3
+          ),
+          selected = 0
+        )
+      ),
       
-      
-      
+    box(title = "date range", width = 4, solidHeader = TRUE, status = "primary",
+    dateRangeInput('dateRange2',
+                   label = paste ('select a time span'),
+                   #start = Sys.Date() - 3, end = Sys.Date() + 3,
+                   min = as.Date("1999-01-01"), max = Sys.Date() - 1,
+                   separator = " - ", format = "dd/mm/yy",
+                   startview = 'year', language = 'en', weekstart = 1
     )
   )
-)
+  
+  
+  
+  ), 
+  
+  fluidRow(
+    box(title = "date", width = 4, solidHeader = TRUE, status = "primary",
+        sliderInput("year", "time span", 1999, 2019, value = c(1999, 2019),
+                    sep = "")
+        )
+  ))
+    
+    
+    
+  
+
 
 # We'll save it in a variable `ui` so that we can preview it in the console
 ui <- dashboardPage(
   dashboardHeader(title = "CRAN"),
-  dashboardSidebar(),
+  dashboardSidebar(collapsed= TRUE),
   body
 )
